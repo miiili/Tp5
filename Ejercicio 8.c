cd152b033a83ed741b4a8d26d94c3ce101287545
MILISOLIS <136858089+miiili@users.noreply.github.com>
2023-07-18 12:03:18 -0300

    Add files via upload

---
+++ Ejercicio 8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+int funcion(int px1, int px2, int py1, int py2){
+	int difx;
+	difx= px2-px1;
+	int dify;
+	dify= py2-py1;
+	int suma;
+	suma=pow(difx,2)+pow(dify,2);
+	int mod;
+	mod=sqrt(suma);
+	printf("La diferencia entre los puntos es: %d",mod);
+}
+int main(){
+	int px1;
+	int px2;
+	int py1;
+	int py2;
+	
+	printf("Ingrese el valor de x del primer punto:\n");
+	scanf("%d",&px1);
+	printf("Ingrese el valor de y del primer punto:\n");
+	scanf("%d",&py1);
+	printf("Ingrese el valor de x del segundo punto:\n");
+	scanf("%d",&px2);
+	printf("Ingrese el valor de y del segundo punto:\n");
+	scanf("%d",&py2);
+	funcion(px1,px2,py1,py2);
+	return(0);
+}
+
