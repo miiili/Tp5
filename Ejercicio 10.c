85a30cdad6778865a34d9f9322dafd8abc269141
MILISOLIS <136858089+miiili@users.noreply.github.com>
2023-08-06 23:26:17 -0300

    Add files via upload

---
+++ Ejercicio 10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+int funcion (int dia, int mes, int anio){
+	
+	if (anio%4==0){
+		if (anio%100==0){
+			if (anio%400==0){
+				
+			}
+			else{
+				//NO bisiesto 
+			}
+		}
+		else{
+			//bisiesto
+		}
+	}	
+	else{
+		//NO bisiesto 
+	}
+	
+	return 0;
+}
+	
+	int main{
+		int dia;
+		int mes;
+		int anio;
+		
+		printf ("Ingrese el dia:\n");
+		scanf ("%d"&dia);
+		printf ("Ingrese el mes:\n");
+		scanf ("%d"&mes);
+		printf ("Ingrese el a?o:\n");
+		scanf ("%d"&anio);
+		
+	}
